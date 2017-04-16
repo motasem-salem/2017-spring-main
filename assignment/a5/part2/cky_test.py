af496729a5fd94545b6fae0136bc9519e18b8213
jameskunz <james@kunz.ca>
2017-04-16 13:06:56 -0700

    More (& more robust) unit tests.

--- assignment/a5/part2/cky_test.py
+++ assignment/a5/part2/cky_test.py
@@ -1,6 +1,9 @@
 import cky
 
 import collections
+import pcfg
+from nltk import Tree
+import numpy as np
 import unittest
 
 class MockGrammar(object):
@@ -9,6 +12,35 @@ class MockGrammar(object):
         self.parsing_index.update(parsing_index)
 
 class TestParsing(unittest.TestCase):
+
+    # TODO(jameskunz): Move these tests into pcfg_test.py.
+    # Leaving here now to avoid pushing changes to ipynb after assignment release.
+    def test_pcfg(self):
+        o = pcfg.PCFG()
+        tree = Tree('S', (Tree('NP', ('foo',)), Tree('VP', ('bar',))))
+
+        o.update_counts(tree)
+        self.assertSetEqual(
+                set([(p, 1) for p in tree.productions()]),
+                set(o.production_counts.iteritems()))
+        self.assertSetEqual(set([(p.lhs(), 1) for p in tree.productions()]),
+                set(o.lhs_counts.iteritems()))
+        o.update_counts(tree)
+
+        tree = Tree('S', (Tree('VP', ('foo',)), Tree('NP', ('bar',))))
+        o.update_counts(tree)
+        o.update_counts(tree)
+        self.assertEqual(6, len(o.production_counts))
+        for count in o.production_counts.itervalues():
+            self.assertEqual(2, count)
+        self.assertEqual(3, len(o.lhs_counts))
+        for count in o.lhs_counts.itervalues():
+            self.assertEqual(4, count)
+
+        o.compute_scores()
+        for production, score in o.scored_productions.iteritems():
+            self.assertAlmostEqual(-0.69314718055, score, msg='%s' % production)
+        
 
     def test_failing_rule_application(self):
         chart = cky.make_chart()
@@ -19,6 +51,10 @@ class TestParsing(unittest.TestCase):
         self.assertFalse(cky.CKY_apply_preterminal_rules(
                 'the rock'.split(), grammar, chart))
 
+    def finite_keys(self, cell):
+        return set([pos for pos, pt in cell.iteritems()
+            if not np.isinf(pt.logprob())])
+
     def test_rule_application(self):
         chart = cky.make_chart()
         grammar = MockGrammar({
@@ -27,12 +63,14 @@ class TestParsing(unittest.TestCase):
             ('DT', 'N'): [('NP', -1), ('VP', -300)],
             ('DT', 'V'): [('VP', -2)]})
 
+        sentence = 'the potato'.split()
+
         # Verify preterminal rule application.
         self.assertTrue(cky.CKY_apply_preterminal_rules(
-                'the potato'.split(), grammar, chart))
+            sentence, grammar, chart))
 
         self.assertSetEqual(set([(0, 1), (1, 2)]), set(chart.keys()))
-        self.assertSetEqual(set(['DT']), set(chart[(0, 1)].keys()))
+        self.assertSetEqual(set(['DT']), self.finite_keys(chart[(0, 1)]))
         self.assertEqual(chart[(0, 1)]['DT'].label(), 'DT')
         self.assertEqual(chart[(0, 1)]['DT'].logprob(), -4)
         self.assertEqual(len(chart[(0, 1)]['DT'].leaves()), 1)
@@ -50,14 +88,74 @@ class TestParsing(unittest.TestCase):
         self.assertEqual(chart[(1, 2)]['V'].leaves()[0], 'potato')
 
         # Verify binary rule application.
-        cky.CKY_apply_binary_rules(2, grammar, chart)
+        cky.CKY_apply_binary_rules(len(sentence), grammar, chart)
         self.assertSetEqual(set([(0, 1), (1, 2), (0, 2)]), set(chart.keys()))
-        self.assertEqual(set(chart[(0, 2)].keys()), set(['NP', 'VP']))
+        self.assertEqual(self.finite_keys(chart[(0, 2)]), set(['NP', 'VP']))
         self.assertEqual(chart[(0, 2)]['NP'].label(), 'NP')
         self.assertEqual(chart[(0, 2)]['NP'].logprob(), -8)
         self.assertEqual(chart[(0, 2)]['VP'].label(), 'VP')
         self.assertEqual(chart[(0, 2)]['VP'].logprob(), -26)
         self.assertEqual(chart[(0, 2)]['VP'][1].label(), 'V')
+
+    def test_rule_application_three_words(self):
+        chart = cky.make_chart()
+        grammar = MockGrammar({
+            ('James',): [('N', -4)],
+            ('was',): [('V', -3), ('DT', -20)],
+            ('hungry',): [('JJ', -5), ('N', -21)],
+            ('N', 'JJ'): [('NP', -1), ('VP', -300)],
+            ('N', 'V'): [('NP', -300)],
+            ('N', 'DT'): [('NP', -51)],
+            ('V', 'JJ'): [('VP', -7)],
+            ('N', 'VP'): [('S', -1)],
+            ('JJ', 'N'): [('NP', -2)]})
+
+        sentence = 'James was hungry'.split()
+
+        # Verify preterminal rule application.
+        self.assertTrue(cky.CKY_apply_preterminal_rules(
+                sentence, grammar, chart))
+
+        self.assertSetEqual(set([(0, 1), (1, 2), (2, 3)]), set(chart.keys()))
+        self.assertSetEqual(set(['N']), self.finite_keys(chart[(0, 1)]))
+        self.assertEqual(chart[(0, 1)]['N'].label(), 'N')
+        self.assertEqual(chart[(0, 1)]['N'].logprob(), -4)
+        self.assertEqual(len(chart[(0, 1)]['N'].leaves()), 1)
+        self.assertEqual(chart[(0, 1)]['N'].leaves()[0], 'James')
+
+        self.assertSetEqual(set(['DT', 'V']), self.finite_keys(chart[(1, 2)]))
+        self.assertEqual(chart[(1, 2)]['V'].label(), 'V')
+        self.assertEqual(chart[(1, 2)]['V'].logprob(), -3)
+        self.assertEqual(len(chart[(1, 2)]['V'].leaves()), 1)
+        self.assertEqual(chart[(1, 2)]['V'].leaves()[0], 'was')
+
+        self.assertEqual(chart[(1, 2)]['DT'].label(), 'DT')
+        self.assertEqual(chart[(1, 2)]['DT'].logprob(), -20)
+        self.assertEqual(len(chart[(1, 2)]['DT'].leaves()), 1)
+        self.assertEqual(chart[(1, 2)]['DT'].leaves()[0], 'was')
+
+        self.assertSetEqual(set(['N', 'JJ']), self.finite_keys(chart[(2, 3)]))
+        self.assertEqual(chart[(2, 3)]['JJ'].label(), 'JJ')
+        self.assertEqual(chart[(2, 3)]['JJ'].logprob(), -5)
+        self.assertEqual(len(chart[(2, 3)]['JJ'].leaves()), 1)
+        self.assertEqual(chart[(2, 3)]['JJ'].leaves()[0], 'hungry')
+
+        self.assertEqual(chart[(2, 3)]['N'].label(), 'N')
+        self.assertEqual(chart[(2, 3)]['N'].logprob(), -21)
+        self.assertEqual(len(chart[(2, 3)]['N'].leaves()), 1)
+        self.assertEqual(chart[(2, 3)]['N'].leaves()[0], 'hungry')
+
+        # Verify binary rule application.
+        cky.CKY_apply_binary_rules(len(sentence), grammar, chart)
+        self.assertSetEqual(
+                set([(0, 1), (1, 2), (0, 2), (2, 3), (1, 3), (0, 3)]),
+                set(chart.keys()))
+        self.assertEqual(self.finite_keys(chart[(0, 2)]), set(['NP']))
+        self.assertEqual(self.finite_keys(chart[(1, 3)]), set(['VP']))
+        self.assertEqual(self.finite_keys(chart[(0, 3)]), set(['S']))
+        self.assertEqual(chart[(0, 2)]['NP'].logprob(), -4 -20 -51)
+        self.assertEqual(chart[(1, 3)]['VP'].logprob(), -3 -5 -7)
+        self.assertEqual(chart[(0, 3)]['S'].logprob(), -4 -3 -5 -7 -1)
 
 
 if __name__ == '__main__':
